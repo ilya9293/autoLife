7514c24da07c6553ad3a83b29539daf3f3b2db9c
ilya9293 <ilya9293@ukr.net>
2023-06-14 17:26:17 +0300

    add ticker

--- js/script.js
+++ js/script.js
@@ -146,9 +146,7 @@ document.addEventListener("DOMContentLoaded", function () {
   showNextParagraphs();
 });
 
-const inputs = document.querySelectorAll(
-  ".form-feedback__input[type=tel]"
-);
+const inputs = document.querySelectorAll(".form-feedback__input[type=tel]");
 const dropDownContainer = document.querySelectorAll(
   ".form-feedback__wraper_phone"
 );
@@ -156,8 +154,7 @@ const dropDownContainer = document.querySelectorAll(
 if (inputs && dropDownContainer) {
   iti = [...inputs].map((el, idx) => {
     return window.intlTelInput(el, {
-      utilsScript:
-        "/js/utils.js",
+      utilsScript: "/js/utils.js",
       preferredCountries: ["ua", "us", "gb"],
       separateDialCode: true,
       dropdownContainer: dropDownContainer[idx],
@@ -165,67 +162,116 @@ if (inputs && dropDownContainer) {
   });
 }
 
-document.querySelectorAll(".select").forEach(select => {
-   const selectOption = select.querySelectorAll("option");
-   const selectOptionLength = selectOption.length;
-   const selectedOption = Array.from(selectOption).find(option => option.selected);
-   const duration = 450;
- 
-   const wrapper = document.createElement("div");
-   wrapper.className = "select";
-   select.parentNode.insertBefore(wrapper, select);
- 
-   select.style.display = "none";
-   wrapper.appendChild(select);
- 
-   const newSelect = document.createElement("div");
-   newSelect.className = "new-select";
-   newSelect.textContent = select.querySelector("option:disabled").textContent;
-   wrapper.appendChild(newSelect);
- 
-   const selectHead = newSelect;
- 
-   const selectList = document.createElement("div");
-   selectList.className = "new-select__list";
-   wrapper.appendChild(selectList);
- 
-   for (let i = 1; i < selectOptionLength; i++) {
-     const selectItem = document.createElement("div");
-     selectItem.className = "new-select__item";
-     const span = document.createElement("span");
-     span.textContent = selectOption[i].textContent;
-     selectItem.appendChild(span);
-     selectItem.dataset.value = selectOption[i].value;
-     selectList.appendChild(selectItem);
-   }
- 
-   const selectItems = selectList.querySelectorAll(".new-select__item");
-   selectList.style.display = "none";
-   selectHead.addEventListener("click", () => {
-     if (!selectHead.classList.contains("on")) {
-       selectHead.classList.add("on");
-       selectList.style.display = "block";
-       selectItems.forEach(item => {
-         item.addEventListener("click", function () {
-           const chooseItem = this.dataset.value;
-           select.value = chooseItem;
-           selectedOption.removeAttribute("selected");
-           selectOption.forEach(option => {
-             if (option.value === chooseItem) {
-               option.setAttribute("selected", "selected");
-             }
-           });
-           selectHead.textContent = this.querySelector("span").textContent;
-           selectList.style.display = "none";
-           selectHead.classList.remove("on");
-         });
-       });
-     } else {
-       selectHead.classList.remove("on");
-       selectList.style.display = "none";
-     }
-   });
- });
- 
- 
- 
+document.querySelectorAll(".select").forEach((select) => {
+  const selectOption = select.querySelectorAll("option");
+  const selectOptionLength = selectOption.length;
+  const selectedOption = Array.from(selectOption).find(
+    (option) => option.selected
+  );
+  const duration = 450;
+
+  const wrapper = document.createElement("div");
+  wrapper.className = "select";
+  select.parentNode.insertBefore(wrapper, select);
+
+  select.style.display = "none";
+  wrapper.appendChild(select);
+
+  const newSelect = document.createElement("div");
+  newSelect.className = "new-select";
+  newSelect.textContent = select.querySelector("option:disabled").textContent;
+  wrapper.appendChild(newSelect);
+
+  const selectHead = newSelect;
+
+  const selectList = document.createElement("div");
+  selectList.className = "new-select__list";
+  wrapper.appendChild(selectList);
+
+  for (let i = 1; i < selectOptionLength; i++) {
+    const selectItem = document.createElement("div");
+    selectItem.className = "new-select__item";
+    const span = document.createElement("span");
+    span.textContent = selectOption[i].textContent;
+    selectItem.appendChild(span);
+    selectItem.dataset.value = selectOption[i].value;
+    selectList.appendChild(selectItem);
+  }
+
+  const selectItems = selectList.querySelectorAll(".new-select__item");
+  selectList.style.display = "none";
+  selectHead.addEventListener("click", () => {
+    if (!selectHead.classList.contains("on")) {
+      selectHead.classList.add("on");
+      selectList.style.display = "block";
+      selectItems.forEach((item) => {
+        item.addEventListener("click", function () {
+          const chooseItem = this.dataset.value;
+          select.value = chooseItem;
+          selectedOption.removeAttribute("selected");
+          selectOption.forEach((option) => {
+            if (option.value === chooseItem) {
+              option.setAttribute("selected", "selected");
+            }
+          });
+          selectHead.textContent = this.querySelector("span").textContent;
+          selectList.style.display = "none";
+          selectHead.classList.remove("on");
+        });
+      });
+    } else {
+      selectHead.classList.remove("on");
+      selectList.style.display = "none";
+    }
+  });
+});
+
+// Получение ссылок на контейнеры с помощью querySelectorAll
+const containers = document.querySelectorAll(".ticker");
+
+// Массив из двух слов
+const words = ["Компанія доставки", "Авто-лайф_PL"];
+
+// Создание временного span для вычисления ширины
+const tempSpan = document.createElement("span");
+tempSpan.style.visibility = "hidden";
+tempSpan.style.whiteSpace = "nowrap";
+
+// Установка содержимого временного span
+const wordIndex = 0; // Берем первое слово из массива
+tempSpan.textContent = `${words[wordIndex]}`;
+
+// Вычисление ширины span с вложенным контентом
+document.body.appendChild(tempSpan); // Добавляем временный span в DOM
+const spanWidth = tempSpan.offsetWidth; // Получаем вычисленную ширину span
+document.body.removeChild(tempSpan); // Удаляем временный span из DOM
+
+// Определение максимальной ширины контейнеров
+let maxWidth = 0;
+containers.forEach((container) => {
+  maxWidth = Math.max(maxWidth, container.offsetWidth);
+});
+
+// Создание контейнера для бегущей строки
+const marqueeContainer = document.createElement("div");
+marqueeContainer.className = "marquee-container";
+
+// Вычисление необходимого количества span
+const totalSpanCount = Math.ceil(maxWidth / spanWidth);
+
+// Создание спанов с вложенными элементами <svg>
+for (let i = 0; i < totalSpanCount; i++) {
+  const span = document.createElement("span");
+  const wordIndex = i % words.length;
+  span.innerHTML = `${words[wordIndex]} <svg width="18" height="17" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12.7078 11.2809L17.6982 7.32406L11.383 6.50569L9.16122 0.538539L6.4296 6.29004L0.0864488 6.56786L4.69502 10.9352L2.98832 17.0694L8.5774 14.019L13.8834 17.5402L12.7078 11.2809Z" fill="#F2D53C"/>
+  </svg>`;
+  marqueeContainer.appendChild(span);
+}
+
+// Добавление бегущей строки в каждый контейнер
+containers.forEach((container) => {
+  container.appendChild(marqueeContainer.cloneNode(true));
+});
+
+
