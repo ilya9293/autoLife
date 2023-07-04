d5b31180f7e9d8dc11ad8b24549c72034892649f
ilya9293 <ilya9293@ukr.net>
2023-07-04 11:41:55 +0300

    add policy

--- js/script.js
+++ js/script.js
@@ -496,13 +496,13 @@ const policyLink = document.querySelector(".policy__link");
 
 switch (curentLng) {
   case "ua":
-    policyLink.setAttribute("href", "1");
+    policyLink.setAttribute("href", "../policy.htm");
     break;
   case "pl":
-    policyLink.setAttribute("href", "2");
+    policyLink.setAttribute("href", "../policy.htm");
     break;
   case "en":
-    policyLink.setAttribute("href", "3");
+    policyLink.setAttribute("href", "../policy.htm");
     break;
   default:
     break;
